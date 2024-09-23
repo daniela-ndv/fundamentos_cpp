d5cd4d68ca7a1c1269b1001f15b2292d3250348d
daniela-ndv <danielandv67@gmail.com>
2024-09-23 19:46:42 -0300

    Funções - passagem por valor e por referência

--- funcoes/passagem_valor_referencia.cpp
+++ funcoes/passagem_valor_referencia.cpp
@@ -8,7 +8,32 @@ Deixe bem claro que uma altera o valor somente dentro da função (valor) e a ou
 
 using namespace std;
 
-int main(){
+void cubo_por_valor(int);
+void cubo_por_referencia(int&);
+
+int main()
+{
+    int numero;
+    cout << "Informe um número inteiro: ";
+    cin >> numero;
+
+    cout<<"\nAntes de cubo_por_valor: num = "<<numero<<endl;
+    cubo_por_valor(numero);
+    cout<<"Depois de cubo_por_valor: num = "<<numero<<endl;
+    cout<<"\nAntes de cubo_por_referencia: num = "<<numero<<endl;
+    cubo_por_referencia(numero);
+    cout<<"Depois de cubo_por_referencia: num = "<<numero<<endl;
 
     return 0;
+}
+
+void cubo_por_valor(int num)
+{
+    num = num*num*num;
+    cout<<"Durante cubo_por_valor: num = "<<num<<endl;
+}
+
+void cubo_por_referencia(int& num){
+    num = num*num*num;
+    cout<<"Durante cubo_por_referencia: num = "<<num<<endl;
 }
